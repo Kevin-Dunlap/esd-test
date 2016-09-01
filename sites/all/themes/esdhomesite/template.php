2fcce97f50fc5856fb974cc4f891b0f66927a696
Kevin.Dunlap <kevindunlap@wnsmlu-nw749fxy.na.corp.ipgnetwork.com>
2016-09-01 14:10:13 -0400

    added hero node template and preprocess function to template.php

--- sites/all/themes/esdhomesite/template.php
+++ sites/all/themes/esdhomesite/template.php
@@ -8,3 +8,11 @@
 		}
 		return theme_links($variables);
 	}
+
+	function esdhomesite_preprocess_page(&$vars, $hook) {
+		if (isset($vars['node']->type)) {
+	    	// If the content type's machine name is "my_machine_name" the file
+	    	// name will be "page--my-machine-name.tpl.php".
+	    	$vars['theme_hook_suggestions'][] = 'page__' . $vars['node']->type;
+		}
+	}
---
+++ sites/all/themes/esdhomesite/templates/nodes/node--hero.tpl.php
@@ -0,0 +1,6 @@
+<?php
+	$file = file_load($node->field_hero_image['und'][0]['fid']);
+?>
+
+<div class="row expanded hero-container" style="background:url('<?php print file_create_url($file->uri); ?>') center top;background-size:cover;height:430px">
+</div>
